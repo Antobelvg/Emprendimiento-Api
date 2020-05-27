54e811f7e97a0735343dfbe4f966a9d664f37b98
Antobelvg <66036922+Antobelvg@users.noreply.github.com>
2020-05-27 17:21:15 -0500

    Update server.js

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.use( morgan( "dev" ) );
 // 		});
 // });
 
-app.get( "/api/emprendimiento", ( req, res, next ) => {
+app.get( "/api/emp", ( req, res, next ) => {
 	VPList.get()
 		.then( vicepresidencia => {
 			return res.status( 200 ).json( vicepresidencia );
